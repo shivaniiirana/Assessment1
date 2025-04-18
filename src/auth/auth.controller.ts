55004798c4861d489a223e90dd15e7e9c9c3d715
shivaniiirana <shivani.rana@appinventiv.com>
2025-04-18 19:56:02 +0530

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-// src/auth/auth.controller.ts
+
 import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
